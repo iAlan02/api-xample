c7833d45ada304b55f520b3bc4af85e32e41c207
Alan Ruiseco <V138856@global.ual.com>
2023-11-30 16:00:56 -0600

    Fixed routing order

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router() // express router
 const stateRouter = require('./statesRouter') // states router
 const cityRouter = require('./citiesRouter') // cities router
 
+router.use('/state', stateRouter) // use states router
+router.use('/city', cityRouter) // use cities router
 router.use('/', (req, res) => { // root route
     res.send('Hello World!') // send hello world
 })
-router.use('/state', stateRouter) // use states router
-router.use('/city', cityRouter) // use cities router
 
 module.exports = router // export router
